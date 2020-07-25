3b80434d84d4c0175a6cd55f95583d7a24478594
Vladislav Daragan <Vladislav Daragan>
2020-07-25 10:56:30 +0200

    Initial Commit

---
+++ Light/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Light
+//
+//  Created by Vladislav Daragan on 25.07.20.
+//  Copyright © 2020 Vladislav Daragan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
